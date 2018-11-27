b939338019d9341bbd86e8fe9a1d7d2b11783939
Christian <chris@NYChrisMacBook-Pro.local>
2018-11-27 11:30:39 +0900

    updates

--- home.php
+++ home.php
@@ -1,8 +1,20 @@
 <?php  
 include("auth.php");
 include('server.php'); 
+$upload_dir = 'uploads/..';
+ 
 ?>
 <?php 
+	if(isset($_GET['id'])){
+		$id = $_GET['id'];
+		$sql = "select * from info where id=".$id;
+		$result = mysqli_query($db, $sql);
+		if(mysqli_num_rows($result) > 0){
+			$row = mysqli_fetch_assoc($result);
+		}else{
+			$errorMsg = 'Could not select a record';
+		}
+	}
 	if (isset($_GET['edit'])) {
 		$id = $_GET['edit'];
 		$update = true;
@@ -13,6 +25,7 @@ include('server.php');
 			$name = $record['name'];
 			$address = $record['address'];
 			$id = $record['id'];
+			$image = $record['image'];
 		}
 	}
 ?>
@@ -21,6 +34,14 @@ include('server.php');
 <head>
 	<title>CRUD: CReate, Update, Delete PHP MySQL</title>
 	<link rel="stylesheet" type="text/css" href="styles.css">
+	<style>
+	    table,tr,th,td
+	    {
+	    	border: 1px solid black;
+	    	
+	    }
+		
+	</style>
 
 </head>
 <body>
@@ -35,30 +56,33 @@ include('server.php');
 <?php $results = mysqli_query($db, "SELECT * FROM info"); ?>
 
 <table>
-	<thead>
+	
 		<tr>
+			<th>Id</th>
 			<th>Name</th>
 			<th>Address</th>
 			<th colspan="2">Action</th>
-		</tr>
-	</thead>
+			<th>Image</th>
+		
+	    </tr>
+	
+
 	
 	<?php while ($row = mysqli_fetch_array($results)) { ?>
 		<tr>
+			<td><?php echo $row['id']; ?></td>
 			<td><?php echo $row['name']; ?></td>
 			<td><?php echo $row['address']; ?></td>
-			<td>
-				<a href="home.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
-			</td>
-			<td>
-				<a href="server.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
-			</td>
+			<td><a href="home.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a></td>
+			<td><a href="server.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a></td>
+			<td><img src="<?php echo $upload_dir.$row['image'] ?>" height="40"></td>
+
 		</tr>
+	
 	<?php } ?>
 </table>
 
-
-	<form method="post" action="server.php" >
+    <form action="" method="post" enctype="multipart/form-data">
 		
     <input type="hidden" name="id" value="<?php echo $id; ?>">
 
@@ -70,6 +94,14 @@ include('server.php');
 			<label>Address</label>
 			<input type="text" name="address" value="<?php echo $address; ?>">
 		</div>
+		<div class="input-group">
+			<label>Image</label>
+			<input type="file" name="file">
+           
+    
+        </div>
+
+
 		<div class="input-group">
 <?php if ($update == true): ?>
 	<button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
--- server.php
+++ server.php
@@ -1,6 +1,7 @@
 <?php 
+ $upload_dir = 'uploads/..';
 	//session_start();
-	$db = mysqli_connect('localhost', 'root', 'mysql', 'register');
+	$db = mysqli_connect('localhost', 'root', '', 'register');
 
 	// initialize variables
 	$name = "";
@@ -8,14 +9,57 @@
 	$id = 0;
 	$update = false;
 	$edit_state = false;
+	
+
 
 	if (isset($_POST['save'])) {
 		$name = $_POST['name'];
 		$address = $_POST['address'];
+		$imgName = $_FILES['file']['name'];
+		$imgTmp = $_FILES['file']['tmp_name'];
+		$imgSize = $_FILES['file']['size'];
+		$imgSize = $_FILES['file']['size'];
+		
+		
+if(empty($name)){
+			$errorMsg = 'Please input name';
+		}elseif(empty($address)){
+			$errorMsg = 'Please input address';
+		}elseif(empty($imgName)){
+			$errorMsg = 'Please select file';
+		}else{
+			//get image extension
+			$imgExt = strtolower(pathinfo($imgName, PATHINFO_EXTENSION));
+			//allow extenstion
+			$allowExt  = array('jpeg', 'jpg', 'png', 'gif');
+			//random new name for photo
+			$userPic = time().'_'.rand(1000,9999).'.'.$imgExt;
+			//check a valid image
+			if(in_array($imgExt, $allowExt)){
+				//check image size less than 5MB
+				if($imgSize < 5000000){
+					move_uploaded_file($imgTmp ,$upload_dir.$userPic);
+				}else{
+					$errorMsg = 'Image too large';
+				}
+			}else{
+				$errorMsg = 'Please select a valid image';
+			}
+		}
 
-		mysqli_query($db, "INSERT INTO info (name, address) VALUES ('$name', '$address')"); 
-		$_SESSION['msg'] = "Address saved"; 
-		header('location: home.php');// redirect to index page after inserting
+		//check upload file not error than insert data to database
+		if(!isset($errorMsg)){
+			$sql = "insert into info(name, address, image)
+					values('".$name."', '".$address."', '".$userPic."')";
+			$result = mysqli_query($db, $sql);
+			if($result){
+				$successMsg = 'New record added successfully';
+				header('location:home.php');
+			}else{
+				$errorMsg = 'Error '.mysqli_error($db);
+			}
+		}
+
 	}
 
     // update records
@@ -23,18 +67,64 @@
 	$id = $_POST['id'];
 	$name = $_POST['name'];
 	$address = $_POST['address'];
+	$imgName = $_FILES['file']['name'];
+	$imgTmp = $_FILES['file']['tmp_name'];
+	$imgSize = $_FILES['file']['size'];
 
-	mysqli_query($db, "UPDATE info SET name='$name', address='$address' WHERE id=$id");
-	$_SESSION['msg'] = "Address updated!"; 
-	header('location: home.php');
-    
-    }
+		if(empty($name)){
+			$errorMsg = 'Please input name';
+		}elseif(empty($address)){
+			$errorMsg = 'Please input address';
+		}
+
+		//udate image if user select new image
+		if($imgName){
+			//get image extension
+			$imgExt = strtolower(pathinfo($imgName, PATHINFO_EXTENSION));
+			//allow extenstion
+			$allowExt  = array('jpeg', 'jpg', 'png', 'gif');
+			//random new name for photo
+			$userPic = time().'_'.rand(1000,9999).'.'.$imgExt;
+			//check a valid image
+			if(in_array($imgExt, $allowExt)){
+				//check image size less than 5MB
+				if($imgSize < 5000000){
+					//delete old image
+					unlink($upload_dir.$row['image']);
+					move_uploaded_file($imgTmp ,$upload_dir.$userPic);
+				}else{
+					$errorMsg = 'Image too large';
+				}
+			}else{
+				$errorMsg = 'Please select a valid image';
+			}
+		}else{
+			//if not select new image - use old image name
+			$userPic = $row['image'];
+		}
+
+		//check upload file not error than insert data to database
+		if(!isset($errorMsg)){
+			$sql = "update info
+									set name = '".$name."',
+										address = '".$address."',
+										image = '".$userPic."'
+					where id=".$id;
+			$result = mysqli_query($db, $sql);
+			if($result){
+				$successMsg = 'New record updated successfully';
+				header('location:home.php');
+			}else{
+				$errorMsg = 'Error '.mysqli_error($db);
+			}
+		}
+
+	}
     if (isset($_GET['del'])) {
 	$id = $_GET['del'];
 	mysqli_query($db, "DELETE FROM info WHERE id=$id");
-	$_SESSION['msg'] = "Address deleted!"; 
+	$_SESSION['message'] = "Address deleted!"; 
 	header('location: home.php');
-    
-    }
+}
 
 ?>   
